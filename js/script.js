15a4b2f8a84a00afb864894ca464544a7f9950a5
iambasanta <iambasantarai@gmail.com>
2023-08-13 19:17:14 +0545

    Add key press sound

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@
 const helpMessage = document.getElementById("help-message");
 const displayKeyCode = document.getElementById("key-code");
 const displayKey = document.getElementById("key");
+const audio = document.getElementById("audio");
 
 document.addEventListener("keydown", function (event) {
   helpMessage.classList.add("hide");
+
+  audio.currentTime = 0;
+  audio.play();
+
   displayKeyCode.innerHTML = event.keyCode;
   displayKey.innerHTML = `Pressed key: ${event.key}`;
 });
